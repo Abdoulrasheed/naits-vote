dee86548e09f7aca3ae690ba6ca116db7ca19b01
abdoulrasheed <abdulrasheedibraheem47@yahoo.com>
2018-04-17 20:18:05 +0100

    added Responsive admin UI

--- naits_voting/settings.py
+++ naits_voting/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
 	'voting',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- naits_voting/urls.py
+++ naits_voting/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 
 urlpatterns = [
 	url(r'^', include('voting.urls')),
+    url(r'^jet/', include('jet.urls', 'jet')),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/accounts/login'}),
