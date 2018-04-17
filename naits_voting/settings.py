f530e0a1d133cb765f44950a029d2ea11238174d
abdoulrasheed <abdulrasheedibraheem47@yahoo.com>
2018-04-17 20:42:46 +0100

    fix static root issues

--- naits_voting/settings.py
+++ naits_voting/settings.py
@@ -112,3 +112,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 
 AUTH_USER_MODEL = 'voting.User'
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
