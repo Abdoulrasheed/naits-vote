8ae465ad231d277bb5367dfb8496a7a4ca42a3f3
abdoulrasheed <abdulrasheedibraheem47@yahoo.com>
2018-04-19 13:01:20 +0100

    Added more style to conform with admin style

--- naits_voting/settings.py
+++ naits_voting/settings.py
@@ -20,12 +20,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
 	'voting',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
