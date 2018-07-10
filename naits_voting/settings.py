89ddd6790ca167f46a47925ad0157386f67be8ed
abdoulrasheed <abdulrasheedibraheem47@yahoo.com>
2018-07-11 00:16:25 +0100

    Update settings to use channels

--- naits_voting/settings.py
+++ naits_voting/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'bitpoint.voting',
     'bitpoint.messenger',
     'bitpoint.articles',
+    'bitpoint.discussion',
 ]
 
 MIDDLEWARE = [
@@ -147,20 +148,64 @@ MEDIA_URL = '/media/'
 TAGGIT_CASE_INSENSITIVE = True
 
 # REDIS setup
-REDIS_URL = env.str('REDIS_URL', default=('localhost', 6379))
+redis_host = os.environ.get('REDIS_HOST', 'localhost')
+
 # Channels configuration
 
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'asgi_redis.RedisChannelLayer',
+#         'CONFIG': {
+#             'hosts': [REDIS_URL, ],
+#         },
+#         'ROUTING': 'config.routing.channel_routing',
+#     }
+# }
+
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'asgi_redis.RedisChannelLayer',
-        'CONFIG': {
-            'hosts': [REDIS_URL, ],
+    "default": {
+        # This example app uses the Redis channel layer implementation channels_redis
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [(redis_host, 6379)],
         },
-        'ROUTING': 'config.routing.channel_routing',
-    }
+    },
 }
+
+ASGI_APPLICATION = 'naits_voting.routing.application'
 
 # EMAIL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+
+
+
+##### Project-specific settings
+
+NOTIFY_USERS_ON_ENTER_OR_LEAVE_ROOMS = True
+
+MSG_TYPE_MESSAGE = 0  # For standard messages
+MSG_TYPE_WARNING = 1  # For yellow messages
+MSG_TYPE_ALERT = 2  # For red & dangerous alerts
+MSG_TYPE_MUTED = 3  # For just OK information that doesn't bother users
+MSG_TYPE_ENTER = 4  # For just OK information that doesn't bother users
+MSG_TYPE_LEAVE = 5  # For just OK information that doesn't bother users
+
+MESSAGE_TYPES_CHOICES = (
+    (MSG_TYPE_MESSAGE, 'MESSAGE'),
+    (MSG_TYPE_WARNING, 'WARNING'),
+    (MSG_TYPE_ALERT, 'ALERT'),
+    (MSG_TYPE_MUTED, 'MUTED'),
+    (MSG_TYPE_ENTER, 'ENTER'),
+    (MSG_TYPE_LEAVE, 'LEAVE'),
+)
+
+MESSAGE_TYPES_LIST = [
+    MSG_TYPE_MESSAGE,
+    MSG_TYPE_WARNING,
+    MSG_TYPE_ALERT,
+    MSG_TYPE_MUTED,
+    MSG_TYPE_ENTER,
+    MSG_TYPE_LEAVE,
+]
